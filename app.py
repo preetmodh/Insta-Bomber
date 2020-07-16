799ac7bd36140585ca89c6c13d21c2ab6efcf4b9
preetmodh <54811605+preetmodh@users.noreply.github.com>
2020-07-16 11:53:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask,request,render_template
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import os
+#to add selenium on heroku
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# Now you can start using Selenium
 app = Flask(__name__, template_folder='template')
 @app.route("/")
 def index():
